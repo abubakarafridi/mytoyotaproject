e19e18b57cf723c78a8fe3f3e53687263e9b3184
Your Name <you@example.com>
2022-07-30 16:50:05 +0500

    done

--- flutter_toyota/lib/items/file.dart
+++ flutter_toyota/lib/items/file.dart
@@ -26,7 +26,7 @@ class FilePickerAppState extends State<FilePickerApp> {
         paths = await FilePicker.GetMultiFilePath(
             fileType : FileType.any,
             allowedExtensions: extensions);
-aklj      } else {
+      } else {
         path = await FilePicker.getFilePath(
             fileType : FileType.any,
             allowedExtensions: extensions);
